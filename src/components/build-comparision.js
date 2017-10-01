40ce7675e8cd1a480e607c6b7b7e63880c2b5316
Varun Vachhar <varunvachhar@gmail.com>
2017-09-30 21:11:31 -0400

    wip

--- src/components/build-comparision.js
+++ src/components/build-comparision.js
@@ -11,23 +11,23 @@ export const BuildComparision = props => (
     <tbody className="lh-copy">
       <tr>
         <td className="tl bb pv3 pl3">🚫</td>
-        <td className="bb pv3">942 kB</td>
+        <td className="bb pv3">930 kB</td>
       </tr>
       <tr>
         <td className="tl bb pv3 pl3">📤</td>
-        <td className="bb pv3">844 kB</td>
+        <td className="bb pv3">786 kB</td>
       </tr>
       <tr>
         <td className="tl bb pv3 pl3">📤 + ✂️</td>
-        <td className="bb pv3">828 kB</td>
+        <td className="bb pv3">768 kB</td>
       </tr>
       <tr>
         <td className="tl bb pv3 pl3">📤 + ✂️ + 🎯</td>
-        <td className="bb pv3">767 kB</td>
+        <td className="bb pv3">768 kB</td>
       </tr>
       <tr>
         <td className="tl bb pv3 pl3">📤 + ✂️ + 🎯 + 🤐</td>
-        <td className="bb pv3">173 kB</td>
+        <td className="bb pv3">154 kB</td>
       </tr>
     </tbody>
   </table>
